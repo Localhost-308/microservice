6b9aa7137e16fe713c261aa2bf735cdbfacc4037
Lukas <lkfernando12345@gmail.com>
2025-04-08 17:31:01 -0300

    feat Task 79: deixando os comentarios em ingles no arquivo auth_manager

--- src/main/security/auth_manager.py
+++ src/main/security/auth_manager.py
@@ -6,14 +6,14 @@ from werkzeug.exceptions import Unauthorized
 class AuthManager:
     def __check_jwt(self):
         """
-        Verifica se o cookie de acesso está presente na requisição.
-        Retorna True se o cookie estiver presente, caso contrário False.
+        Checks if the access JWT token is present in the session.
+        Returns True if the JWT token is present, otherwise False.
         """
         return 'access_token' in session
 
     def __get_jwt(self):
         """
-        Obtém o JWT do cookie.
+        Retrieves the JWT token.
         """
         access_token = session.get('access_token')
         if access_token:
@@ -27,7 +27,7 @@ class AuthManager:
 
     def __verify_jwt(self):
         """
-        Verifica se o JWT está presente e válido.
+        Verifies if the JWT token is present and valid.
         """
         token = self.__get_jwt()
         if not token:
@@ -39,8 +39,8 @@ class AuthManager:
     
     def __call__(self, fn):
         """
-        Decorador para proteger as rotas que requerem autenticação.
-        Verifica se o JWT está presente no cookie, se não, redireciona para o login.
+        Decorator to protect routes that require authentication.
+        Checks if the JWT token is present in the session, if not, redirects to login.
         """
         @wraps(fn)
         def wrapper(*args, **kwargs):
