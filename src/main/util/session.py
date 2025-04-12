5f9fb6cbdfd184be3aa5c50c0e1d9436aff2aacf
Lukas <lkfernando12345@gmail.com>
2025-04-12 12:23:32 -0300

    feat Task 79: adicionando algumas mensagens em util/

---
+++ src/main/util/session.py
@@ -0,0 +1,9 @@
+# Sessão relacionada à API externa
+API_PUBLIC_KEY = 'external_api_public_key'
+API_CALLBACK = 'external_api_callback'
+API_USER_EMAIL = 'external_api_user_email'
+API_TOKEN = 'external_api_token'
+API_USER_ID = 'external_api_user_id'
+
+# Sessão interna
+ACCESS_TOKEN = 'access_token'
